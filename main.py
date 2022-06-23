f4a588897f072c16ae410c33eb7ea8757ec9a335
darkn07 <darkn4754+mail@gmail.com>
2022-06-23 17:05:48 +0300

    Added parse to post list function

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from readmail import *
+from parsemail import *
 
 if __name__ == '__main__':
   POP3_SERVER = 'pop.gmail.com'
   PORT = '995'
   EMAIL = 'dark0666.emailboard@gmail.com'
-  print(readMail(POP3_SERVER, PORT, EMAIL))
+  msgList = readMail(POP3_SERVER, PORT, EMAIL)
+  postList = parseToPostList(msgList)
+  print(postList)
+
+  print(postList[0].subject)
+  print(postList[0].message)
+  print(postList[1].subject)
+  print(postList[1].message)
---
+++ parsemail.py
@@ -0,0 +1,19 @@
+class Post:
+  def __init__(self, subject, message):
+    self.subject = subject
+    self.message = message
+
+def parseToPostList(msgList):
+  postList = []
+  for i in msgList:
+    msg = i.decode()
+    subject = ''
+    message = ''
+    if 'Subject: ' in msg:
+      subject = msg.split(':')[1].strip()
+      for i in range(1, len(msgList)):
+        if "Subject: " not in msgList[i].decode():
+          message = message + msgList[i].decode() + '\n'
+        else: break
+      postList.append(Post(subject, message.strip()))
+  return postList
