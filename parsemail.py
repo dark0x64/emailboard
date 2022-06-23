ec6627bbc7bb79f0db308b514d90706c9417a5c8
darkn07 <darkn4754+mail@gmail.com>
2022-06-23 18:18:23 +0300

    Fix bug in parseToPostList

--- parsemail.py
+++ parsemail.py
@@ -5,15 +5,19 @@ class Post:
 
 def parseToPostList(msgList):
   postList = []
+  count = 1
   for i in msgList:
     msg = i.decode()
     subject = ''
     message = ''
     if 'Subject: ' in msg:
       subject = msg.split(':')[1].strip()
-      for i in range(1, len(msgList)): # start from latter
+      for i in range(count, len(msgList)): # start from latter
         if "Subject: " not in msgList[i].decode():
           message = message + msgList[i].decode() + '\n'
-        else: break
+          count += 1
+        else:
+          count += 1
+          break
       postList.append(Post(subject, message.strip()))
   return postList
