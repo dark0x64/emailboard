d7b7fea703d9a0a13e14ea001b3e6bcf80fedb64
darkn07 <darkn4754+mail@gmail.com>
2022-06-24 10:54:47 +0300

    Print incoming posts if debug value is true

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ if __name__ == '__main__':
   POP3_SERVER = 'pop.gmail.com'
   PORT = '995'
   EMAIL = 'dark0666.emailboard@gmail.com'
+  DEBUG = True
   try:
     msgList = readMail(POP3_SERVER, PORT, EMAIL)
     postList = parseToPostList(msgList)
-    for i in range(0, len(postList)):
-      print(postList[i].subject)
-      print(postList[i].message)
+    if DEBUG == True:
+      for i in range(0, len(postList)): # print incoming posts
+        print(postList[i].subject)
+        print(postList[i].message)
   except Exception as e:
     print(e)
