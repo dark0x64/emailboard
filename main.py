cbb1305d98ed9298dafc00d03209105b83f14487
darkn07 <darkn4754+mail@gmail.com>
2022-06-24 10:51:59 +0300

    Catch exceptions in main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ if __name__ == '__main__':
   POP3_SERVER = 'pop.gmail.com'
   PORT = '995'
   EMAIL = 'dark0666.emailboard@gmail.com'
-  msgList = readMail(POP3_SERVER, PORT, EMAIL)
-  postList = parseToPostList(msgList)
-  for i in range(0, len(postList)):
-    print(postList[i].subject)
-    print(postList[i].message)
+  try:
+    msgList = readMail(POP3_SERVER, PORT, EMAIL)
+    postList = parseToPostList(msgList)
+    for i in range(0, len(postList)):
+      print(postList[i].subject)
+      print(postList[i].message)
+  except Exception as e:
+    print(e)
